0c24c087024a4692d4879ef8a088d4e93f3cdda4
Cenia Ayala <ceniabayala@gmail.com>
2024-09-23 20:22:27 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+//Exercises
+
+//1. 
+//let some_month = let some_Month
+//function the Month() = function theMonth()
+//let current-month = let current-Month
+//let summer_month = let summer_Month
+//let MyLibrary-function = let myLibrary-Function
+
+//2.
+//numeric literal expression: 1, -1, 0
+//string literal expression: 'Hello, world!'
+//Boolean literal expression: True or false
+//null literal expression: null
+
+//3
+//complex/ variable expressions : let x= a + b  or let x; x=123
+
+//4
+//let firstName = 
+//let lastName =
+//let address =
+//let city =
+//let state =
+//let zipCode = 
+//let yourAge = 
+//let referralSource = 
+//let mayWeContactYou = 
+
+//5
+//let firstName = 'Cenia'
+//let lastName = 'Ayala'
+//let zipCode = 91999
+
+//6
+const firstName = 'Cenia';
+const zipCode = 91999;
+const sum = firstName + zipCode;
+console.log(sum);
+
+
+String(23);
+
+
+
+
+
