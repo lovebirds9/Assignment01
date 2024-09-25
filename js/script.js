a4b43b2c21e30bca48ce6a448e95480c6b78f69c
Cenia Ayala <ceniabayala@gmail.com>
2024-09-25 09:56:52 -0500

    last commit

--- js/script.js
+++ js/script.js
@@ -17,30 +17,120 @@
 //complex/ variable expressions : let x= a + b  or let x; x=123
 
 //4
-//let firstName = 
-//let lastName =
-//let address =
-//let city =
-//let state =
-//let zipCode = 
-//let yourAge = 
-//let referralSource = 
-//let mayWeContactYou = 
+//let firstName 
+//let lastName 
+//let address 
+//let city 
+//let state 
+//let zipCode 
+//let yourAge 
+//let referralSource 
+//let mayWeContactYou 
 
 //5
 //let firstName = 'Cenia'
-//let lastName = 'Ayala'
-//let zipCode = 91999
+//var lastName = 'Ayala'
+//const zipCode = 91999
 
 //6
-// this will be our second commit
-const firstName = 'Cenia';
-const zipCode = 91999;
-const sum = firstName + zipCode;
-console.log(sum);
+// let yourAge = 30
+//alert ('Cenia is ' + yourAge + ' years old.')
+//Chrome: Cenia is 30 years old. 
+
+//7
+//let bool = false
+// alert ('It is ' + bool +' .')
+//Chrome: It is false. 
+//and
+//let num = 10
+//let bool = true
+//let together = num + bool
+// together 
+// Chrome : 11 
+
+//8
+//Not valid if we removed the I'm to i am then it is valid. 
+
+//9
+//let x = null
+//console.log(x)
+//Chrome: null
+//and 
+//let x = 0/0
+//Chrome: undefined
+
+//10
+// string: console.log(typeof 'Hello' === 'string'); Consule: true
+//number: console.log(typeof 30 === 'number'); Consule: true
+//Boolean: console.log(typeof true === 'boolean') Consule: true
+//object: console.log(type of null === 'object') Console: true
+//undefined: console.log(type of 'x = null' === 'undefined') Console: false
+
+//11
+//let firstName = 'Cenia'
+//let lastName = 'Ayala'
+//alert ('Hello '+ firstName +' '+ lastName +', welcome to JavaScript class!')
+
+//12
+//let name = 'Cenia Ayala'
+//alert ('Hello '+ name +', welcome to JavaScript class!')
+
+//13
+//let course = 'JavaScript'
+//alert('Hello Cenia Ayala, welcome to the '+ course +' class!')
+
+//14
+//let name = 'Hello Zak Ruvalcaba.\nWelcome to the JavaScript class!'
+//console.log(name)
+
+//15
+//let name = prompt('What is your name?')
+//Box: What is your name? I put Cenia and hit ok
+//Console: name
+// answer: Cenia
+
+//16
+//let course = prompt('What class are you taking?')
+//Box: What class are you taking? I input math and hit ok 
+//console : course
+// answer: math
+
+//17
+//let x = 10
+//let y = 20
+//sum = x + y 
+//sum
+//Console: 30
+
+//18
+//let x = 20
+// x += 20
+//console: 40
+
+//19
+//let x = 20 
+// x *= 5
+//console: 100
+
+//20
+// let x = 20%3
+// x = 2
+
+//21
+//let total = prompt ("What is your total of students?");
+//let aStudents = prompt ('How many have an A in class?');
+//let bStudents = prompt ('How many have an B in class?');
+//let cStudents = prompt ('How many have an C in class?');
+//let results = aStudents + bStudents + cStudents / total;
+//document.write(`You have $results.toFixed(2) students that are passing your class.`)
+
+//22
 
 
-String(23);
+
+
+
+
 
 
 
